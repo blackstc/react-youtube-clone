aed9088ec6f91329b9bf3fff12117efc170d08c3
Blackstock <charlieblackstock@gmail.com>
2016-01-19 18:56:50 -0700

    added video detail and description components

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search-bar';
+import VideoList from './components/video-list';
+import VideoDetail from './components/video-detail';
+const API_KEY = 'AIzaSyCiZdnEi08exA-xZOlN8QL__ybT303b2DE';
+
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'gaming'}, (videos) => {
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoDetail video={this.state.videos[0]} />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
+}
+
+ReactDom.render(<App />, document.querySelector('.container'));
